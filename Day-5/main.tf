887a2deddef6945e05bb4a11a3efd8778a69e12c
sreelekhab77 <133074494+SreelekhaB77@users.noreply.github.com>
2025-07-15 16:35:04 +0530

    Update main.tf

--- Day-5/main.tf
+++ Day-5/main.tf
@@ -8,7 +8,7 @@ variable "cidr" {
 }
 
 resource "aws_key_pair" "example" {
-  key_name   = "terraform-demo-abhi"  # Replace with your desired key name
+  key_name   = "my-aws-key"  # Replace with your desired key name
   public_key = file("~/.ssh/id_rsa.pub")  # Replace with the path to your public key file
 }
 
@@ -73,7 +73,7 @@ resource "aws_security_group" "webSg" {
 }
 
 resource "aws_instance" "server" {
-  ami                    = "ami-0261755bbcb8c4a84"
+  ami                    = "ami-020cba7c55df1f615"
   instance_type          = "t2.micro"
   key_name      = aws_key_pair.example.key_name
   vpc_security_group_ids = [aws_security_group.webSg.id]
@@ -103,4 +103,3 @@ resource "aws_instance" "server" {
     ]
   }
 }
-
